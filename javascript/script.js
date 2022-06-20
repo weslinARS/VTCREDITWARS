3a0078ec9081967004ae4c7536b482ceabc6fba4
weslinARS <weslin.rodriguez@est.ulsa.edu.ni>
2022-06-20 09:52:25 -0600

    dd

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-const formulario = document.getElementById('formulario');
+const formulario = document.getElementById("formulario");
 const inputs = document.querySelectorAll("#formulario");
 /* 
 ?La función recibirá como argumento un número de tarjeta de
@@ -8,19 +8,35 @@ const inputs = document.querySelectorAll("#formulario");
 ?(no pueden ser todos el mismo digito).
 */
 
-
-const verificarLetra = (caracter ) =>{
-    let car = caracter.toUpperCase().charCodeAt(0);
-    return car > 64 && car < 91 ; 
-}; 
-formulario.addEventListener('submit',(e) =>{
-    tarjeta= document.getElementById("numTarjeta").value;
-    console.log("tarjeta ingresada ->"+ tarjeta);
+const verificarLetra = (caracter) => {
+  let car = caracter.toUpperCase().charCodeAt(0);
+  return car > 64 && car < 91;
+}; /*
+inputs.forEach((input) => {
+	input.addEventListener('keyup',validarFormulario );
+	input.addEventListener('blur', validarFormulario);
+});*/
+var regex = /\-/i;
+formulario.addEventListener("submit", (e) => {
+    tarjeta = document.getElementById("numTarjeta").value;
+    let cont = 0;
+    var x = 0;
+    let tarjetaModificada = [""];
     for (let i = 0; i < tarjeta.length; i++) {
-        if(!(verificarLetra(tarjeta[i]) || (tarjeta >= 48 && tarjeta <= 57)  )){
-            tarjeta.slice(i,i+1);
-        }
+    console.log(verificarLetra(tarjeta[i]));
+    if (verificarLetra(tarjeta[i]) == false && isNaN(tarjeta[i])) {
+        console.log("i -> "+ i+" x-> "+x);
+      //! si lo que se encuentra es un guión la cadena se parte y se copia en una nueva variable
+        let aux = tarjeta.slice(x, i); 
+        x = i +1 ; 
+        tarjetaModificada = tarjeta.concat(aux);
+        console.log("cadena modifica -> "+tarjetaModificada);
+        console.log("Ahora x vale = [" + x + "]");
     }
-    alert(tarjeta);
+    
+    }
+    console.log(tarjeta.replace("-", "*"));
+    console.log("hay un total de " + cont + " guiones");
+    console.log(tarjeta);
     e.preventDefault();
 });
